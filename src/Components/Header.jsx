800204b118356a8e448108e0052354356c0a11ac
Vansh Kapadia <144871405+vanshkapadia11@users.noreply.github.com>
2025-07-07 18:32:41 +0530

    Added The ABout Section!! -- And More To GO

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,12 +9,7 @@ const Header = () => {
         <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
           <h2>
             <a href="">
-              <img
-                src="/public/images/logo.svg"
-                alt=""
-                width={40}
-                height={40}
-              />
+              <img src="/images/logo.svg" alt="" width={40} height={40} />
             </a>
           </h2>
           <div className="relative md:justify-self-center">
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
             <div className="flex items-center gap-3">
               <figure className="img-box w-9 h-9 rounded-lg">
                 <img
-                  src="/public/images/avatar-1.jpg"
+                  src="/images/avatar-1.jpg"
                   className="img-cover"
                   width={40}
                   height={40}
@@ -40,7 +40,7 @@ const Hero = () => {
           <div className="hidden lg:block">
             <figure className="w-full max-w-[480px] ml-auto bg-gradient-to-t from-sky-400 via-25% via-sky-400/40 to-65% rounded-[60px] overflow-hidden">
               <img
-                src="/public/images/hero-banner.png"
+                src="/images/hero-banner.png"
                 className="w-full"
                 width={656}
                 height={800}
