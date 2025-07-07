8e169bd203866fb953a55d322691d53414bec6f3
Vansh Kapadia <144871405+vanshkapadia11@users.noreply.github.com>
2025-07-07 18:32:41 +0530

    Added The About Section -- More To GO

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
+import About from "./Components/About";
 function App() {
   return (
     <>
       <Header />
       <main>
         <Hero />
+        <About />
       </main>
     </>
   );
